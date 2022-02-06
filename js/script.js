64f3b0fe68f718ab0926de6c4c8979700fa45dc1
Dmitry Trakhimovich <dmitrytrakhimovich@google.com>
2022-02-06 22:03:19 +0300

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,70 +1,91 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const removePromo = document.querySelectorAll('.promo__adv img');
-removePromo.forEach((item)=> {
-    item.remove()
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if (favorite) {
+                console.log("Добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);
+    
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
-
-const poster = document.querySelector('.promo__bg'),
-      newGenre = poster.querySelector('.promo__genre'),
-      removeFilms = document.querySelector('.promo__interactive-list')
-newGenre.textContent = 'драма';
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-removeFilms.innerHTML = '';
-
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i)=> {
-    removeFilms.innerHTML += `
-    <li class="promo__interactive-item">${i+1} ${film}
-    <div class="delete"></div>
-</li>
-    `;
-})
-
-
-
-const menuDB = {
-    pink: [
-        "меню из JS №1",
-        "меню из JS №2",
-        "меню из JS №3",
-        "меню из JS №4",
-        "меню из JS №5"
-    ]
-};
-const menuL = document.querySelector('.promo__menu-list')
-menuL.innerHTML = '';
-menuDB.pink.forEach((i)=> {
-    menuL.innerHTML += `
-    <ul class="promo__menu-list1">
-      <li><a class="promo__menu-item" href="#">${i}</a></li>
-</ul>
-    
-    
-    `;
-})
-
